7ce2949c7d7bf18dd9d59a2f21a596f0551f2c94
Varuneshwar Nath Mathur <vmathur4@sapient.com>
2018-05-09 16:37:04 +0100

    kafka stream send message

--- stock-message-generator/src/main/java/com/sapient/retail/service/streamkafka/web/StockDataController.java
+++ stock-message-generator/src/main/java/com/sapient/retail/service/streamkafka/web/StockDataController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import com.sapient.retail.service.streamkafka.model.StockData;
 
 @RestController
 public class StockDataController {
@@ -18,11 +19,9 @@ public class StockDataController {
     @GetMapping("/stock")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void stockdata(@RequestParam("message") String message) {
-//        StockData stockdata = StockData.builder()
-//                .message(message)
-//            .timestamp(System.currentTimeMillis())
-//            .build();
-//
-//        stockService.sendStockData(stockdata);
+       StockData stockdata = StockData.builder().skuid(message)
+            .build();
+
+        stockService.sendStockData(stockdata);
     }
 }
