19e3db7be354cef570893bff86e605b49d54667b
Raghav Arora <rarora9@sapient.com>
2018-05-15 21:59:57 +0100

    Added missing files for Mongo repo and simple Pojo (needs more changes, will do later)

---
+++ stock-message-generator/src/main/java/com/sapient/retail/service/streamkafka/repository/StockRepository.java
@@ -0,0 +1,13 @@
+package com.sapient.retail.service.streamkafka.repository;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+import com.sapient.retail.service.streamkafka.model.Stock;
+
+/**
+ * Stock Repository for Reactive MongoDB interactions
+ * @author ragarora
+ */
+public interface StockRepository extends ReactiveMongoRepository<Stock, Integer> {
+	
+}
