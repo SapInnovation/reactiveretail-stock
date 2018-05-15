e47aceab68e160ea0a7b3846821a507a89918946
Raghav Arora <rarora9@sapient.com>
2018-05-15 23:08:45 +0100

    Updated with new JSON Structure for API and for Listener and feed to Mongo

--- stock-message-generator/src/main/java/com/sapient/retail/service/streamkafka/repository/StockRepository.java
+++ stock-message-generator/src/main/java/com/sapient/retail/service/streamkafka/repository/StockRepository.java
@@ -1,8 +1,6 @@
 package com.sapient.retail.service.streamkafka.repository;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-<<<<<<< HEAD
 import com.sapient.retail.service.streamkafka.model.ProductStockInfo;
 
 /**
@@ -10,14 +8,5 @@ import com.sapient.retail.service.streamkafka.model.ProductStockInfo;
  * @author ragarora
  */
 public interface StockRepository extends ReactiveMongoRepository<ProductStockInfo, Integer> {
-=======
-import com.sapient.retail.service.streamkafka.model.Stock;
-
-/**
- * Stock Repository for Reactive MongoDB interactions
- * @author ragarora
- */
-public interface StockRepository extends ReactiveMongoRepository<Stock, Integer> {
->>>>>>> db927a509a0b14c2f2e978de3e0af25788f04e6e
 	
 }
