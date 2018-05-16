406620716a0f4d2442a4ae812200102ce2190d41
Raghav Arora <rarora9@sapient.com>
2018-05-16 16:03:09 +0100

    Removed Lombok integration and renamed POJO beans

--- stock-stream-processor/src/main/java/com/sapient/retail/service/streamkafka/service/StockDataListener.java
+++ stock-stream-processor/src/main/java/com/sapient/retail/service/streamkafka/service/StockDataListener.java
@@ -1,21 +1,21 @@
 package com.sapient.retail.service.streamkafka.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
-import com.sapient.retail.service.streamkafka.model.ProductStockInfo;
-import com.sapient.retail.service.streamkafka.model.StockInfo;
+import com.sapient.retail.service.streamkafka.model.ProductStock;
+import com.sapient.retail.service.streamkafka.model.SkuStock;
 import com.sapient.retail.service.streamkafka.repository.StockRepository;
 import com.sapient.retail.service.streamkafka.stream.StockDataStreams;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Component
-@Slf4j
 public class StockDataListener {
 	
 	private final StockRepository stockRepository;
+	private Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	public StockDataListener(final StockRepository stockRepository) {
         this.stockRepository = stockRepository;
@@ -23,23 +23,23 @@ public class StockDataListener {
 
 	/**
 	 * This method listens to INPUT Kafka topic and persists data in Mongo repository 
-	 * in a block mode and doesn't wait for any mono subscription.
-	 * @param stockInfo ProductStockInfo
+	 * in a blocking mode and doesn't wait for any mono subscriptions.
+	 * @param newProductStockDetails ProductStock
 	 */
 	@StreamListener(StockDataStreams.INPUT)
-    public void handleGreetings(@Payload ProductStockInfo newProductStockDetails) {
+    public void handleStockDataFromTopic(@Payload ProductStock newProductStockDetails) {
 
-		ProductStockInfo existingProductStockDetailsUpdated = null;
+		ProductStock existingProductStockDetailsUpdated = null;
 		boolean stockExists = stockRepository.existsById(newProductStockDetails.getUpc()).block();
 
 		if (stockExists) {
-			ProductStockInfo existingProductStockDetails = stockRepository.findById(newProductStockDetails.getUpc()).block();
+			ProductStock existingProductStockDetails = stockRepository.findById(newProductStockDetails.getUpc()).block();
 			log.debug("New Prod Stock Details: {}", newProductStockDetails);
 
-			for (StockInfo newStockInfo: newProductStockDetails.getStock()) {
+			for (SkuStock newSkuStock: newProductStockDetails.getStock()) {
 				existingProductStockDetails.getStock().removeIf(
-						stockInfo -> 
-							stockInfo.getLocationId().equals(newStockInfo.getLocationId()));
+						skuStock -> 
+							skuStock.getLocationId().equals(newSkuStock.getLocationId()));
 			}
 			log.debug("Existing Prod Stock Details after filter: {}", existingProductStockDetails);
 			existingProductStockDetails.getStock().addAll(newProductStockDetails.getStock());
--- stock-stream-processor/src/main/java/com/sapient/retail/service/streamkafka/service/StockDataService.java
+++ stock-stream-processor/src/main/java/com/sapient/retail/service/streamkafka/service/StockDataService.java
@@ -1,31 +1,31 @@
 package com.sapient.retail.service.streamkafka.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MimeTypeUtils;
 
-import com.sapient.retail.service.streamkafka.model.ProductStockInfo;
+import com.sapient.retail.service.streamkafka.model.ProductStock;
 import com.sapient.retail.service.streamkafka.stream.StockDataStreams;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Service
-@Slf4j
 public class StockDataService {
     private final StockDataStreams stockdatastreams;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public StockDataService(StockDataStreams stockdatastreams) {
         this.stockdatastreams = stockdatastreams;
     }
 
-    public void sendStockData(final ProductStockInfo stockdata) {
-        log.info("Sending stock data {}", stockdata);
+    public void sendStockData(final ProductStock productStock) {
+    	logger.info("Sending stock data {}", productStock);
 
         MessageChannel messageChannel = stockdatastreams.outboundStockData();
         messageChannel.send(MessageBuilder
-                .withPayload(stockdata)
+                .withPayload(productStock)
                 .setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
                 .build());
     }
