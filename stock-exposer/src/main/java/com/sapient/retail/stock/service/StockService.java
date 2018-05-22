c2b0a6d209983e49c6da04e8228cb8fdf0f596e9
Himanshu Tomar <htomar@sapient.com>
2018-05-22 16:35:20 +0100

    Cleaned up Service, Controller. Added Test cases

--- stock-exposer/src/main/java/com/sapient/retail/stock/service/StockService.java
+++ stock-exposer/src/main/java/com/sapient/retail/stock/service/StockService.java
@@ -1,45 +1,86 @@
 package com.sapient.retail.stock.service;
 
-import java.util.List;
-
+import com.sapient.retail.stock.common.repository.StockRepository;
 import com.sapient.retail.stock.model.StockResponse;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
-public interface StockService {
+import java.util.List;
 
-	/**
-	 * Method to lookup Database for Stock information for requested Product
-	 *
-	 * @param productId the product Id
-	 * @return Mono object containing a {@link List} of Stock {@link StockResponse}
-	 */
-	Mono<List<StockResponse>> productStock(String productId);
+@Service
+public class StockService {
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
-	/**
-	 * Method to lookup Database for Stock information for requested UPC
-	 *
-	 * @param upc the SKU upc
-	 * @return Mono object containing Stock {@link StockResponse}
-	 */
-	Mono<StockResponse> skuStock(Long upc);
+    private final StockRepository repository;
+    private final HelperService helper;
 
-	/**
-	 * Method to lookup Database for Stock details for provided ProductId and LocationId
-	 *
-	 * @param productId  the product Id
-	 * @param locationId the store location Id
-	 * @return Mono object containing a {@link List} of Stock {@link StockResponse}
-	 */
-	Mono<List<StockResponse>> productStockForLocation(String productId, Long locationId);
+    /**
+     * Dependency injection based constructor.
+     *
+     * @param repository {@link StockRepository} instance
+     * @param helper     {@link HelperService} for common tasks
+     */
+    public StockService(final StockRepository repository,
+                        final HelperService helper) {
+        this.repository = repository;
+        this.helper = helper;
+    }
 
-	/**
-	 * Method to lookup Database for Stock details for provided UPC and LocationId
-	 *
-	 * @param upc        the SKU upc
-	 * @param locationId the store location Id
-	 * @return Mono<StockResponse> with only requested UPC and locationId, if present
-	 */
-	Mono<StockResponse> skuStockForLocation(Long upc, Long locationId);
+    /**
+     * Method to lookup Database for Stock information for requested Product
+     *
+     * @param productId the product Id
+     * @return Mono object containing a {@link List} of Stock {@link StockResponse}
+     */
+    public Mono<List<StockResponse>> productStock(final String productId) {
+        LOGGER.debug("Fetching stock information for product: " + productId);
+        return repository.findStocksByProductIdOrderByUpc(productId)
+                .switchIfEmpty(helper.stockNotFound())
+                .map(helper::buildFromStock)
+                .collectList();
+    }
 
+    /**
+     * Method to lookup Database for Stock information for requested UPC
+     *
+     * @param upc the SKU upc
+     * @return Mono object containing Stock {@link StockResponse}
+     */
+    public Mono<StockResponse> skuStock(final Long upc) {
+        LOGGER.debug("Fetching stock information for UPC: " + upc);
+        return repository.findStocksByUpc(upc)
+                .switchIfEmpty(helper.stockNotFound())
+                .map(helper::buildFromStock);
+    }
+
+    /**
+     * Method to lookup Database for Stock details for provided ProductId and LocationId
+     *
+     * @param productId  the product Id
+     * @param locationId the store location Id
+     * @return Mono object containing a {@link List} of Stock {@link StockResponse}
+     */
+    public Mono<List<StockResponse>> productStockForLocation(final String productId, final Long locationId) {
+        LOGGER.debug("Fetching stock information for Product: " + productId + " & location: " + locationId);
+        return repository.findStocksByProductIdOrderByUpc(productId)
+                .switchIfEmpty(helper.stockNotFound())
+                .map(stock -> helper.buildFromStock(stock, locationId))
+                .collectList();
+    }
+
+    /**
+     * Method to lookup Database for Stock details for provided UPC and LocationId
+     *
+     * @param upc        the SKU upc
+     * @param locationId the store location Id
+     * @return Mono<StockResponse> with only requested UPC and locationId, if present
+     */
+    public Mono<StockResponse> skuStockForLocation(final Long upc, final Long locationId) {
+        LOGGER.debug("Fetching stock information for UPC: " + upc + " & location: " + locationId);
+        return repository.findStocksByUpc(upc)
+                .switchIfEmpty(helper.stockNotFound())
+                .map(stock -> helper.buildFromStock(stock, locationId));
+    }
 }
