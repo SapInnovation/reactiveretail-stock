360e2aff71cac25a85a749ca96fa750c660e1346
Amit Kumar <akumar105@sapient.com>
2018-05-24 14:43:20 +0100

    missing flag in apt

--- ci/scripts/dockerbuild.sh
+++ ci/scripts/dockerbuild.sh
@@ -7,7 +7,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get install -y docker-ce
 sleep 10s
 docker login -u ${REGISTRY_USER} -p ${REGISTRY_PASSWORD}
 ./gradlew :stock-exposer:build :stock-exposer:docker :stock-exposer:dockerPush --no-daemon
