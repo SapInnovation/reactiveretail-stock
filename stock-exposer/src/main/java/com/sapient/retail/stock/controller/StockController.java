5d82eeff13f319d1154ee9606e51ea46b185d2de
Himanshu Tomar <htomar@sapient.com>
2018-05-31 14:59:59 +0100

    Adding CORS configuration.

--- stock-exposer/src/main/java/com/sapient/retail/stock/controller/StockController.java
+++ stock-exposer/src/main/java/com/sapient/retail/stock/controller/StockController.java
@@ -5,15 +5,13 @@ import com.sapient.retail.stock.common.model.impl.StockResponse;
 import com.sapient.retail.stock.service.StockService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class StockController {
     private final StockService<RetailStock, StockResponse> stockService;
