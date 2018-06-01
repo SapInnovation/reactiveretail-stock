fbbdfdbad4fe6e230232a5eb593055f79403dfbb
Raghav Arora <rarora9@sapient.com>
2018-06-01 17:52:32 +0100

    Update deploy.sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,13 +5,10 @@ apk update
 apk add  sshpass
 apk add openssh
 apk add docker
-service docker start
 
 cd source
 pwd
 ls -ltr
-
-ls -ltrh /opt/
 
 #sshpass -p $USER_PASS scp -r -o StrictHostKeyChecking=no . $USER_NAME@$HOST_NAME:/opt/stock-$SERVICE
 
