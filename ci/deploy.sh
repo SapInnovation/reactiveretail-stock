60a9b7c88bea2b5597af0dd2918b5e70b234747e
Raghav Arora <rarora9@sapient.com>
2018-06-01 16:54:54 +0100

    Update deploy.sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -9,6 +9,6 @@ cd source
 pwd
 ls -ltr
 
-sshpass -p $USER_PASS scp -r -o StrictHostKeyChecking=no . $USER_NAME@$HOST_NAME:/opt/stock-$SERVICE
+#sshpass -p $USER_PASS scp -r -o StrictHostKeyChecking=no . $USER_NAME@$HOST_NAME:/opt/stock-$SERVICE
 
-sshpass -p $USER_PASS ssh -o StrictHostKeyChecking=no $USER_NAME@$HOST_NAME 'export KAFKA_HOST='$KAFKA_HOST' && export DB_HOST='$DB_HOST' && docker stack deploy stock-'$SERVICE' -c /opt/stock-'$SERVICE'/ci/docker-image-deploy-'$SERVICE'.yml'
+#sshpass -p $USER_PASS ssh -o StrictHostKeyChecking=no $USER_NAME@$HOST_NAME 'export KAFKA_HOST='$KAFKA_HOST' && export DB_HOST='$DB_HOST' && docker stack deploy stock-'$SERVICE' -c /opt/stock-'$SERVICE'/ci/docker-image-deploy-'$SERVICE'.yml'
