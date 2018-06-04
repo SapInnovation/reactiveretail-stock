15436f011af655cc27f908480028a0b7877f8789
Raghav Arora <rarora9@sapient.com>
2018-06-04 13:56:48 +0100

    Updated to not deploy but ls for test

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -11,4 +11,5 @@ ls -ltr
 
 sshpass -p $USER_PASS scp -r -o StrictHostKeyChecking=no . $USER_NAME@$HOST_NAME:/opt/stock-$SERVICE
 
-sshpass -p $USER_PASS ssh -o StrictHostKeyChecking=no $USER_NAME@$HOST_NAME 'export KAFKA_HOST='$KAFKA_HOST' && export DB_HOST='$DB_HOST' && docker stack deploy stock-'$SERVICE' -c /opt/stock-'$SERVICE'/ci/docker-image-deploy-'$SERVICE'.yml'
+#sshpass -p $USER_PASS ssh -o StrictHostKeyChecking=no $USER_NAME@$HOST_NAME 'export KAFKA_HOST='$KAFKA_HOST' && export DB_HOST='$DB_HOST' && docker stack deploy stock-'$SERVICE' -c /opt/stock-'$SERVICE'/ci/docker-image-deploy-'$SERVICE'.yml'
+sshpass -p $USER_PASS ssh -o StrictHostKeyChecking=no $USER_NAME@$HOST_NAME 'export KAFKA_HOST='$KAFKA_HOST' && export DB_HOST='$DB_HOST' && docker stack ls stock-'$SERVICE' -c /opt/stock-'$SERVICE'/ci/docker-image-deploy-'$SERVICE'.yml'
